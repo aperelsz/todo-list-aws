359813201c9546d3c6d956eb3324bec0c0079d7f
aperelsz <110926737+aperelsz@users.noreply.github.com>
2023-01-24 17:27:19 -0300

    se adiciona caso test_get_todo_error

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -104,7 +104,14 @@ class TestDatabaseFunctions(unittest.TestCase):
             self.text,
             responseGet['text'])
         print ('End: test_get_todo')
-    
+
+    def test_get_todo_error(self):
+        print ('-----------------------')
+        print ('Start test_get_todo_error')
+        from src.todoList import get_item
+        self.assertRaises(Exception, get_item("", self.dynamodberror))     
+        print('End: test_get_todo_error')
+        
     def test_list_todo(self):
         print ('---------------------')
         print ('Start: test_list_todo')
