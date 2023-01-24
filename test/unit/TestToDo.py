4b2928d7443b04f7057dd1cdb184df5ddd2ca94d
aperelsz <110926737+aperelsz@users.noreply.github.com>
2023-01-24 15:52:47 -0300

    update

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -104,6 +104,14 @@ class TestDatabaseFunctions(unittest.TestCase):
             responseGet['text'])
         print ('End: test_get_todo')
     
+    def test_get_todo_error(self):
+        print ('--------------------')
+        print ('Start: test_get_todo_error')
+        from src.todoList import get_item
+        from src.todoList import put_item
+        self.assertRaises(Exception, get_item("Error"))
+        print ('End: test_get_todo_error')
+    
     def test_list_todo(self):
         print ('---------------------')
         print ('Start: test_list_todo')
